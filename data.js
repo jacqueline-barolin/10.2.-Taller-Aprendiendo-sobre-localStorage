eeac5eaef6f3e0ed3a2a54ab14a1572da4e0414d
jacqueline-barolin.github.io <jacquelinebarolin03@gmail.com>
2024-09-16 23:37:34 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const savedData = localStorage.getItem('dato');
+    const savedData = localStorage.getItem('data');
     document.getElementById('data').textContent = savedData ? savedData : 'No hay datos guardados.';
 });
