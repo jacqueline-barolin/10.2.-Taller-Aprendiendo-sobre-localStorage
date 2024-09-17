adf84b89f9f07f39cf946b73fce33e3af4525f06
Jacqueline-03-mvd <jacquelinebarolin03@gmail.com>
2024-09-16 23:25:22 -0300

    commit push

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const buttonText = document.getElementById('buttonText');
+    
+
+buttonText.addEventListener('click', function(){
+    const inputText = document.getElementById('inputText').value;
+    localStorage.setItem('dato', inputText)
+    document.getElementById('inputText').value = '';
+    window.location.href = 'data.html';
+})
+
+    
+});
