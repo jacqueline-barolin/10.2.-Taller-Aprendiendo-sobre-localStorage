41c5a732f601dec4a9a26cccb0114f623f691317
Jacqueline-03-mvd <jacquelinebarolin03@gmail.com>
2024-09-16 23:28:16 -0300

    commit push

--- data.js
+++ data.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
     const savedData = localStorage.getItem('dato');
-
-const data = document.getElementById('data').textContent = savedData ? savedData: 'No hay datos guardados.';
-
+    document.getElementById('data').textContent = savedData ? savedData : 'No hay datos guardados.';
 });
